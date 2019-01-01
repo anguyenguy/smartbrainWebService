ebd7e372ca718c0037f1782deb538853fcff6eaa
unknown <hdragon1501@gmail.com>
2019-01-01 10:31:07 +0700

    add register

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './Navigation.css';
 
-const Navigation = () => {
-	return (
+const Navigation = ({onRouteChange, isSignedin}) => {
+	if(isSignedin){
+		return (
 			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-				<p className='f3 dim pointer underline yellow link pa3'>Sign Out</p>
+				<p onClick={() => onRouteChange('signin')} className='f3 dim pointer underline yellow link pa3'  >Sign Out</p>
 			</nav>
-	);
+		);		
+	}
+	else{
+		return (
+			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+				<p onClick={() => onRouteChange('signin')} className='f3 dim pointer underline yellow link pa3'  >Sign In</p>
+				<p onClick={() => onRouteChange('register')} className='f3 dim pointer underline yellow link pa3'  >Register</p>
+			</nav>
+		);			
+	}
+
 }
 export default Navigation;
